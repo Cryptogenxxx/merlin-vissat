ac6726c19b7f23c51f62b2f924f8476c2719371e
Cryptogenxxx <161128360+Cryptogenxxx@users.noreply.github.com>
2025-02-06 18:11:21 +0530

    Create About.js

---
+++ About.js
@@ -0,0 +1,13 @@
+// src/components/About.js
+import React from 'react';
+
+const About = () => {
+    return (
+        <div style={{ backgroundColor: '#1F1F1F', color: '#FFFFFF', padding: '20px' }}>
+            <h1 style={{ fontSize: '2.5em' }}>About NEFTIT</h1>
+            <p>Information about the platform and mission.</p>
+        </div>
+    );
+};
+
+export default About;
